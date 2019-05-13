29261e5fe540727d1f0bb10736a4c6eae388bba5
lijianyu <lijianyu@yunloan.net>
2019-05-13 19:46:44 +0800

    springboot auto generate properties document

--- time-enhance/src/main/java/com/ljy/spt/autoconfigure/TimeProperties.java
+++ time-enhance/src/main/java/com/ljy/spt/autoconfigure/TimeProperties.java
@@ -3,13 +3,21 @@ package com.ljy.spt.autoconfigure;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
+ * 时间属性配置类
  * @Author: lijianyu
  * @Date: 2018/7/23 19:47
  */
 public class TimeProperties {
 
+    /**
+     * 时间格式
+     */
     private String dateFormat ="yyyy-MM-dd HH:mm:ss";
 
+    /**
+     *
+     * @return
+     */
     public String getDateFormat() {
         return dateFormat;
     }
